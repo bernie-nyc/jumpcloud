5f2d154c0406134a05b8c1615f9a69cd8360764d
bernie-nyc <bernie@sworks-tech.com>
2024-12-30 07:46:44 -0500

    Create ggreg.ps1

---
+++ GoGuardian/ggreg.ps1
@@ -0,0 +1,29 @@
+# PowerShell Script to Configure Registry Keys for GoGuardian App
+
+# Set LicenseTag Registry Key
+New-Item -Path "HKLM:\SOFTWARE\Policies\GoGuardian" -Force
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\GoGuardian" -Name "LicenseTag" -Value "<Your_License_App_ID>"
+
+# Minimize The GoGuardian App to System Tray on Startup
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\GoGuardian" -Name "StartMinimizedToSystemTray" -Value "true"
+
+# Prevent Users from Changing Proxy Settings
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Internet Explorer\Control Panel" -Force
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Internet Explorer\Control Panel" -Name "Proxy" -Type DWord -Value 1
+
+# Enable Single Sign-On (Optional)
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\GoGuardian" -Name "IdentitySources" -Value "NONE"
+
+# Hardcode a Generic User Account to Sign in (Optional)
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\GoGuardian" -Name "UserEmail" -Value "<Generic_User_Email>"
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\GoGuardian" -Name "UserName" -Value "<Generic_Username>"
+
+# Disable Ephemeral Mode in Microsoft Edge for Shared Devices
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Force
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "ForceEphemeralProfiles" -Type DWord -Value 1
+
+# Turn Off Ephemeral Mode in Google Chrome for Shared Devices
+New-Item -Path "HKLM:\SOFTWARE\Policies\Google\Chrome" -Force
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Google\Chrome" -Name "ForceEphemeralProfiles" -Type DWord -Value 1
+
+Write-Output "Registry keys for GoGuardian App have been configured successfully."
