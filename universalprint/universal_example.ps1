f960ff59cf56b9a7a2c5ffb8a7bb37defab0b9dc
bernie-nyc <bernie@sworks-tech.com>
2025-04-03 07:57:03 -0400

    Update universal_example.ps1

--- universalprint/universal_example.ps1
+++ universalprint/universal_example.ps1
@@ -1,9 +1,9 @@
 # Define variables
 # What is the name of the printer, as it will show up on the user's machine?
 # it should follow the convention NYC|WMS|WLS - Floor - Printer Common Name
-$printerName = "NYC-4th Fl MLS-Library"
+$printerName = "NYC-3rd Floor-Faculty Lounge Ricoh 5300"
 # What is the IP address of that printer?
-$printerIP = "192.168.32.74"
+$printerIP = "192.168.32.73"
 
 # pull down and execute the universal print section
 # the path for Konica is: https://thewindwardschool.io/winpatch/printer/konica_universal.ps1
@@ -11,7 +11,7 @@ $printerIP = "192.168.32.74"
 
 try {
     # Use Invoke-RestMethod to auto-convert response correctly
-    $scriptContent = Invoke-RestMethod -Uri "https://thewindwardschool.io/winpatch/printer/konica_universal.ps1" -UseBasicParsing
+    $scriptContent = Invoke-RestMethod -Uri "https://thewindwardschool.io/winpatch/printer/ricoh_universal.ps1" -UseBasicParsing
 
     # Optional: Confirm content is text before execution (for safety/debugging)
     if ($scriptContent -is [string] -and $scriptContent.Length -gt 0) {
